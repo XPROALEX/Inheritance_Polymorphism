73c6371ab96f59714fba889dcd1790b14c832898
Alex <alex97av@gmail.com>
2023-09-04 13:32:13 +0200

    add Animal class

---
+++ src/Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    private String animalName;
+    public Animal(String animalName){
+        this.animalName=animalName;
+    }
+    public void animalSound(){
+        System.out.println("Default animal sound!");
+    }
+}
